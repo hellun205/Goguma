9db21b04db53755e0e889fdf64766f68ed45cc39
하늘 <hellun515@gmail.com>
2021-05-06 23:59:28 +0900

    Interface Monster, Player

---
+++ Goguma/Game/Player/Interface/IMonster.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goguma.Game.Player.Interface
+{
+  interface IMonster
+  {
+    int Hp { get; set; }  
+    int MaxHp { get; set; }
+
+    int AttDmg { get; set; }
+    int DefPer { get; set; }
+
+    object Skill { get; set; }
+
+    int GivingGold { get; set; }
+    int GivingExp { get; set; }
+
+    void AttackPlayer(IPlayer player);
+
+    void UseSkill();
+
+  }
+}
---
+++ Goguma/Game/Player/Interface/IPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goguma.Game.Player.Interface
+{
+  interface IPlayer
+  {
+    string Name { get; set; }
+
+    object Inventory { get; set; }
+
+    int Hp { get; set; }
+    int Ep { get; set; }
+
+    int MaxHp { get; set; }
+    int MaxEp { get; set; }
+
+    int Level { get; }
+    int Exp { get; set; }
+    int Gold { get; set; }
+
+    int AttDmg { get;  }
+    int ItemDmg { get; set; }
+
+    int DefPer { get; set; }
+
+    object Skill { get; set; }
+
+    void AttackMonster(IMonster moster);
+
+    void UseConsumedItem();
+
+    void UseSkill();
+  }
+}
