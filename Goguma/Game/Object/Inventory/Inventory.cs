468590b229ec961c2acd99b763b9519e73f14876
하늘 <hellun515@gmail.com>
2021-05-07 11:44:09 +0900

    Print Inventory , Test

--- Goguma/Game/Object/Inventory/Inventory.cs
+++ Goguma/Game/Object/Inventory/Inventory.cs
@@ -29,7 +29,7 @@ namespace Goguma.Game.Object.Inventory
       CTexts questionText = new CTexts();
       SelectSceneItems selectSceneItems = new SelectSceneItems();
 
-      questionText = CTexts.Make($"{{인벤토리, cyan}}{{ : }}{{{invenInfo.TypeString}, green}}{{ 를 엽니다. 아이템을 선택 하시오 }}");
+      questionText = CTexts.Make($"{{인벤토리, cyan}}{{ : }}{{{invenInfo.TypeString}, green}}{{ 를 엽니다. }}{{\n    아이템, cyan}}{{를 선택하세요.}}");
       selectSceneItems = new SelectSceneItems();
 
       for (int i = 0; i < invenInfo.TypeItem.Count; i++)
@@ -38,45 +38,6 @@ namespace Goguma.Game.Object.Inventory
       }
 
       SelectItem(itemType, SelectScene(questionText, selectSceneItems) - 1);
-
-      //switch (itemType)
-      //{
-      //  case ItemType.Equipment:
-      //    questionText = CTexts.Make("{인벤토리, cyan}{ : }{장비, green}{ 를 엽니다. }{\n  아이템, cyan}{을 선택하시오.}");
-      //    selectSceneItems = new SelectSceneItems();
-
-      //    for (int i = 0; i < EquipmentItems.Count; i++)
-      //    {
-      //      selectSceneItems.Items.Add(new SelectSceneItem(CTexts.Make($"{{{EquipmentItems[i].Name}}}")));
-      //    }
-
-      //    SelectItem(itemType, SelectScene(questionText, selectSceneItems) - 1);
-      //    break;
-
-      //  case ItemType.Consume:
-      //    questionText = CTexts.Make("{인벤토리, cyan}{ : }{소비, green}{ 를 엽니다. }{\n  아이템, cyan}{을 선택하시오.}");
-      //    selectSceneItems = new SelectSceneItems();
-
-      //    for (int i = 0; i < ConsumeItems.Count; i++)
-      //    {
-      //      selectSceneItems.Items.Add(new SelectSceneItem(CTexts.Make($"{{{ConsumeItems[i].Name}}}")));
-      //    }
-
-      //    SelectItem(itemType, SelectScene(questionText, selectSceneItems) - 1);
-      //    break;
-
-      //  case ItemType.Other:
-      //    questionText = CTexts.Make("{인벤토리, cyan}{ : }{기타, green}{ 를 엽니다. }{\n  아이템, cyan}{을 선택하시오.}");
-      //    selectSceneItems = new SelectSceneItems();
-
-      //    for (int i = 0; i < OtherItems.Count; i++)
-      //    {
-      //      selectSceneItems.Items.Add(new SelectSceneItem(CTexts.Make($"{{{OtherItems[i].Name}}}")));
-      //    }
-
-      //    SelectItem(itemType, SelectScene(questionText, selectSceneItems) - 1);
-      //    break;
-      //}
     }
 
     public void SelectItem(ItemType itemType, int index)
--- Goguma/game/InGame.cs
+++ Goguma/game/InGame.cs
@@ -39,13 +39,29 @@ namespace Goguma.Game
 
       myInventory.EquipmentItems.Add(new Item()
       {
-        Name = CTexts.Make("{테스트아이템1, yellow}{1}{2}{3}{4}{5}{6}")
+        Name = CTexts.Make("{테스트아이템1, yellow}")
       });
 
       myInventory.EquipmentItems.Add(new Item()
       {
         Name = CTexts.Make("{테스트아이템2, red}")
       });
+
+      myInventory.ConsumeItems.Add(new Item()
+      {
+        Name = CTexts.Make("{테스트 소비 아이템1, blue}")
+      });
+
+      myInventory.ConsumeItems.Add(new Item()
+      {
+        Name = CTexts.Make("{테스트포션1, red}")
+      });
+
+      myInventory.OtherItems.Add(new Item()
+      {
+        Name = CTexts.Make("{테스트 기타 아이템, cyan}")
+      });
+
 
 
       myInventory.PrintInventory(Object.Enum.ItemType.Equipment);
