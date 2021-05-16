19692753f16c123e45356fff5e4cd24424a7b70d
5tarlight <developerstarlight@gmail.com>
2021-05-16 23:57:15 +0900

    get npcs

--- Game/Object/Npc/Npcs.cs
+++ Game/Object/Npc/Npcs.cs
@@ -2,6 +2,21 @@ namespace Gogu_Remaster.Game.Object.Npc
 {
   public class Npcs
   {
+    public static NTrader trader = new NTrader();
 
+    public NTrader GetTraderByName(string name)
+    {
+      if (name == trader.Name) return trader;
+      else return null;
+    }
+
+    public NTrader GetTraderByEnum(NpcList npc)
+    {
+      switch (npc)
+      {
+        case NpcList.TRADER: return trader;
+        default: return null;
+      }
+    }
   }
 }
